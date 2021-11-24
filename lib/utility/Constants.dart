3ddfada5c3108bf7498ab61da43ada6996636328
tahamousavi <tahamousavi.sbu@gmail.com>
2021-11-24 20:27:32 +0330

    Select Document Types Api added.

--- lib/utility/Constants.dart
+++ lib/utility/Constants.dart
@@ -20,12 +20,12 @@ class RouteNames {
 
 class Apis {
   Apis._();
-  static const baseUrl = 'https://onlinecheckinapi.abomis.com/';
+  static const baseUrl = '';
   static const login = '/api/login';
   static const signUp = '/api/signUp';
-  static const getTokenUrl = 'api/Execute';
-  static const getInformation = 'api/Execute';
-  static const getDocumentType = 'api/Execute';
+  static const getTokenUrl = '';
+  static const getInformation = '';
+  static const getDocumentType = '';
 
 
 }
